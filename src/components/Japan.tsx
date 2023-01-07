3dc808aca4a503d12fcfdecd5783e31271c7714d
lg <whomsp@gmail.com>
2023-01-07 21:58:32 +0900

    installed styled-components & google-maps/api

--- src/components/Japan.tsx
+++ src/components/Japan.tsx
@@ -1,11 +1,29 @@
 import React, { useEffect } from 'react';
-import './Japan.css';
+import { Link } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import styled from 'styled-components';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+// const Container = styled.div`
+// 	position: absolute;
+// `;
 
 const Japan = () => {
 	return (
-		<div>
-			<h1>hello japan</h1>
-			<p>map</p>
+		<div className="">
+			<Link to="/">home</Link>
+			<ReactPlayer
+				className="react-player"
+				url={
+					'https://www.youtube.com/watch?v=QOjmvL3e7Lc&ab_channel=TokyoLiveCamera'
+				}
+				width="100vw"
+				height="100vh"
+				loop={true}
+				playing={true}
+				muted={true}
+				controls={false}
+			/>
 		</div>
 	);
 };
